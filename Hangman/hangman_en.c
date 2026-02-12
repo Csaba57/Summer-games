9484ecc2b6fb48f012bb895af00cfc76c29e0fab
csaba <frankocsaba5@gmail.com>
2026-02-12 15:34:38 +0100

    Hangman documentation is ready

--- Hangman/hangman_en.c
+++ Hangman/hangman_en.c
@@ -14,10 +14,18 @@ void printLogo();
 
 char* words[] = {"apple", "cliff", "hotspring", "plain", "computer"};
 int lifes = 5;
-char* word;
-char* representation;
+char* word = NULL;
+char* representation = NULL;
 
 
+/*
+The main loop of the game.
+If the number of lifes reaches 0 or if the player manages to guess the letters correctly the game ends.
+In the end the function displays the result.
+
+params: none
+return: 0 (int), if the program ends with no error
+*/
 int main() {
 
     init();
@@ -56,6 +64,14 @@ int main() {
     return 0;
 }
 
+
+/*
+Picking a random word from the wordlist.
+Creating the all '_' character representation afterwards.
+
+params: none
+return: none
+*/
 void init() {
     srand(time(NULL));
 
@@ -69,6 +85,14 @@ void init() {
     }
 }
 
+/*
+Displays the representation of the given word.
+This representation contains both underline and letter characters.
+Thanks to this function the player can see how many letters have been revealed.
+
+param: the pointer of the representation string (char*)
+return: none
+*/
 void printWord(char *representation) {
     for (int i = 0; representation[i] != '\0'; i++) {
 	printf("%c ", representation[i]);
@@ -76,6 +100,13 @@ void printWord(char *representation) {
     puts("");
 }
 
+
+/*
+Prints the state of the hangman image, wich depends on the health of the player.
+
+param: number of lifes (int)
+return: none
+*/
 void printImage(int lifes) {
     char image[200];
     switch (lifes) {
@@ -154,12 +185,24 @@ void printImage(int lifes) {
     printf("%s\n", image);
 }
 
+/*
+Prints the title of the game.
+
+param: none
+return: none
+*/
 void printLogo() {
     printf("===================\n"
 	   "      Hangman      \n"
 	   "===================\n");
 }
 
+/*
+Checks if the given word contains the given letter.
+
+params: a word (char*), the letter to be found (char)
+return: 1 (int), if the word contains the letter, 0 (int) if the letter is not in the word
+*/
 int contains(char* word, char letter) {
     int benne = 0;
     for (int i = 0; i < strlen(word); i++) {
@@ -171,6 +214,12 @@ int contains(char* word, char letter) {
     return benne;
 }
 
+/*
+Changes the characters of the representation according to the given word and the given character.
+
+params: the word to be guessed(char*), the letter to look for (char), the visual representation of the word
+return: none
+*/
 void revealLetters(char* word, char letter, char* representation) {
     for (int i = 0; i < strlen(word); i++) {
 	if (word[i] == letter) {
@@ -179,13 +228,19 @@ void revealLetters(char* word, char letter, char* representation) {
     }
 }
 
+/*
+Checks if the visual representation of the word contatins '_' characters.
+
+param: the visual representation of the word (char*)
+return: 1 (int) if the representation has no '_' characters, 0 (int) otherwise
+*/
 int checkComplete(char* representation) {
-    int komplett = 1;
+    int complete = 1;
     for (int i = 0; i < strlen(representation); i++) {
 	if (representation[i] == '_') {
-	    komplett = 0;
+	    complete = 0;
 	    break;
 	}
     }
-    return komplett;
+    return complete;
 }
